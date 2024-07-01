48a93e9f1d402aec52518fd7e7cdf168116523a4
MarkShileynikov <mark.shiley11@gmail.com>
2024-07-01 12:49:48 +0300

    remove database variable from MainActivity

--- app/src/main/java/com/example/bankingapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bankingapplication/presentation/MainActivity.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.example.bankingapplication.data.database.Database
 import com.example.bankingapplication.presentation.navigation.SetNavigation
 import com.example.bankingapplication.ui.theme.BankingApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     private val viewModel : MainViewModel by viewModels()
-    private lateinit var dataBase: Database
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.statusBarColor = Color.BLACK
